ad715b06aa87888da5e52989004ffc2dd704d177
koreoxy <rayssankn@gmail.com>
2024-05-19 15:33:39 +0700

    add : create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,12 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
 import { RegisterSchema } from "@/schemas";
+import { error } from "console";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -11,5 +15,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validateFields.data;
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashPassword,
+    },
+  });
+
+  //TODO: SEND VERIFICATION TOKEN EMAIL LATER 
+
+  return { success: "User Created!" };
 };
