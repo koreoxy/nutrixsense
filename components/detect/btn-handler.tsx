55c52c4b39db49e1fa91e08a01be6c4641171dde
koreoxy <rayssankn@gmail.com>
2024-07-07 19:56:04 +0700

    fix and delete file folder

--- components/detect/btn-handler.tsx
+++ components/detect/btn-handler.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useRef, MutableRefObject } from "react";
 import { Button } from "@/components/ui/button";
+import { CircleX, SquarePlus, Upload } from "lucide-react";
+import Link from "next/link";
 
 interface ButtonHandlerProps {
   imageRef: MutableRefObject<HTMLImageElement | null>;
@@ -53,25 +55,41 @@ const ButtonHandler: React.FC<ButtonHandlerProps> = ({
         }}
         ref={inputImageRef}
       />
-      <Button
-        onClick={() => {
-          // if not streaming
-          if (streaming === null) inputImageRef.current?.click();
-          // closing image streaming
-          else if (streaming === "image") closeImage();
-          else
-            alert(
-              `Can't handle more than 1 stream\nCurrently streaming : ${streaming}`
-            ); // if streaming video or webcam
-        }}
-        className={
-          isImageUploaded
-            ? "bg-red-500 hover:bg-red-800 text-white"
-            : "text-white"
-        }
-      >
-        {streaming === "image" ? "Remove" : "Upload"} Image
-      </Button>
+      <div className="flex gap-2">
+        <Button
+          onClick={() => {
+            // if not streaming
+            if (streaming === null) inputImageRef.current?.click();
+            // closing image streaming
+            else if (streaming === "image") closeImage();
+            else
+              alert(
+                `Can't handle more than 1 stream\nCurrently streaming : ${streaming}`
+              ); // if streaming video or webcam
+          }}
+          className={
+            isImageUploaded
+              ? "bg-red-500 hover:bg-red-800 text-white w-full"
+              : "text-white w-full"
+          }
+        >
+          {streaming === "image" ? (
+            <>
+              <CircleX className="inline-block mr-2" /> Remove Image
+            </>
+          ) : (
+            <>
+              <Upload className="inline-block mr-2" /> Upload Image
+            </>
+          )}
+        </Button>
+        <Button className="text-white w-full">
+          <Link href="/check-manual">
+            <SquarePlus className="inline-block mr-2" />
+            Check manual
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
